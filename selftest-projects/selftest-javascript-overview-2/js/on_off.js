a214773ead8b332ed46c5c343950d43bbf4fe19f
Pato <pato24193@gmail.com>
2021-11-07 21:45:52 +0700

    pato update 07112021 2145

---
+++ selftest-projects/selftest-javascript-overview-2/js/on_off.js
@@ -0,0 +1,7 @@
+function turnOn() {
+    document.getElementById('imgLight').src = 'img/pic_bulbon.gif';
+}
+
+function turnOff() {
+    document.getElementById('imgLight').src = 'img/pic_bulboff.gif';
+}
